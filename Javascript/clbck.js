16135da2ecccedc085baea04b5dd6cb043cb629e
Chelshiyaa <gautamchelsea639@gmail.com>
2025-07-17 21:49:05 +0530

    done

--- Javascript/clbck.js
+++ Javascript/clbck.js
@@ -1,9 +1,3 @@
-// callback matlb hota hai ki agar humare pass kafi sare function hai or hume vo ekh ke bad ekh nhi chalana hai 
-// mtlb 1 2 3 4 ye function hai or hum chahte hai ki phle 4 chle fir 2 chale fir 3 chale fir 1 chale
-// to hum callback function ka use karte hai
-
-//ab dekho isme humne kya kia hai kii ekh func bnya hai connecttoserver jisme humne ekh or function pass kie hai cbfn thik hai 
-// fir humne kia ki setimeout mai whi function call kr dia jo pass kia tha jo ki fetchcourses ke setimeout mai pass kia hai usme data ki trh pass kia hai 
 
 // Callback ek function hota hai jo dusre function ko complete hone ke baad call hota hai.
 function connectToServer(cbfn){
@@ -28,3 +22,4 @@ connectToServer(function(){
         console.log(data);
     })
 });
+// Callback hell ko avoid karne ke liye hum Promises ka use karte hain.
